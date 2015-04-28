96fbe3483837afb07203708e2ef51193e3491b10
Ernie Salazar <ehsalazar75@gmail.com>
2015-04-27 23:32:51 -0700

    Complete learntonode lesson HELLO WORLD and begin BABY STEPS.

---
+++ core/learnyounode/program.js
@@ -0,0 +1,9 @@
+// HELLO WORLD
+console.log('HELLO WORLD');
+
+// BABY STEPS
+// var args = (process.argv)
+// for (var i = 0; i < args.length; i++) {
+//   console.log(args[i]);
+// };
+// console.log(args);
