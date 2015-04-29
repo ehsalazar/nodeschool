afb4c4c39504459004bcc5cfd11017c05ff8b00c
Ernie Salazar <ehsalazar75@gmail.com>
2015-04-28 20:15:37 -0700

    Complete learnyounode MY FIRST I/O lesson.

--- core/learnyounode/program.js
+++ core/learnyounode/program.js
@@ -5,10 +5,10 @@
 // BABY STEPS
 
 // my solution:
-var args = process.argv.slice(2).map(Number);
-var total = 0
-for(var i in args) { total += args[i]; }
-console.log(total);
+// var args = process.argv.slice(2).map(Number);
+// var total = 0
+// for(var i in args) { total += args[i]; }
+// console.log(total);
 
 // learnyounode solution:
 // var result = 0
@@ -16,3 +16,10 @@ console.log(total);
 //       result += Number(process.argv[i])
 // }
 // console.log(result)
+
+// MY FIRST I/O
+
+var fs = require('fs')
+var file = fs.readFileSync(process.argv[2]);
+var newlines = file.toString().split('\n').length - 1
+console.log(newlines);
