d5309d6abc9ca011f66504c22f389cfbfdf0388c
Ernie Salazar <ehsalazar75@gmail.com>
2015-05-23 14:33:05 -0700

    Complete HTTP CLIENT.

--- core/learnyounode/program.js
+++ core/learnyounode/program.js
@@ -85,3 +85,14 @@
 //     console.log(file)
 //   })
 // })
+
+// HTTP CLIENT
+
+// my solution:
+// var http = require('http');
+
+// http.get(process.argv[2], function(response) {
+//   response.setEncoding('utf8');
+//   response.on('error', console.error);
+//   response.on('data', console.log);
+// });
