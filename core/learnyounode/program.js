c0fe4248956ad6f5ceb74a661e81b991f9b63b79
Ernie Salazar <ehsalazar75@gmail.com>
2015-05-23 13:53:43 -0700

    Complete FILTERED LS.

--- core/learnyounode/program.js
+++ core/learnyounode/program.js
@@ -42,3 +42,13 @@
 //   console.log(lines)
 // })
 
+// FILTERED LS
+// var fs = require('fs')
+// var path = require('path')
+
+// fs.readdir(process.argv[2], function (err, list) {
+//   list.forEach(function (file) {
+//     if (path.extname(file) === '.' + process.argv[3])
+//       console.log(file)
+//   })
+// })
