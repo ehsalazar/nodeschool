7f56422ebb67e2d0f37550335077d5e6d95fe9eb
Ernie Salazar <ehsalazar75@gmail.com>
2015-05-23 15:41:03 -0700

    Complete HTTP FILE SERVER.

--- core/learnyounode/program.js
+++ core/learnyounode/program.js
@@ -273,3 +273,36 @@
 // })
 
 // server.listen(Number(process.argv[2]))
+
+// HTTP FILE SERVER
+// Write an HTTP server that serves the same text file for each request it receives.
+
+// Your server should listen on the port provided by the first argument to your program.
+
+// You will be provided with the location of the file to serve as the second command-line argument. You must use the fs.createReadStream() method to stream the file contents to the response.
+
+// my solution:
+// var http = require('http')
+// var fs = require('fs')
+// var file = process.argv[3]
+// var port = process.argv[2]
+// var stream = fs.createReadStream(file)
+// var server = http.createServer( function (request, response){
+//   if(request){
+//     stream.pipe(response);
+//   }
+// })
+
+// server.listen(port)
+
+// learnyounode solution:
+// var http = require('http')
+// var fs = require('fs')
+
+// var server = http.createServer(function (req, res) {
+//   res.writeHead(200, { 'content-type': 'text/plain' })
+
+//   fs.createReadStream(process.argv[3]).pipe(res)
+// })
+
+// server.listen(Number(process.argv[2]))
