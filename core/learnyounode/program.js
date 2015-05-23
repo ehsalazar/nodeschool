482fddb9ed1f7c4dd6e07f11c46dbe6d468289c9
Ernie Salazar <ehsalazar75@gmail.com>
2015-05-23 15:50:29 -0700

    Complete HTTP UPPERCASERER.

--- core/learnyounode/program.js
+++ core/learnyounode/program.js
@@ -306,3 +306,37 @@
 // })
 
 // server.listen(Number(process.argv[2]))
+
+// HTTP UPPERCASERER
+// Write an HTTP server that receives only POST requests and converts incoming POST body characters to upper-case and returns it to the client.
+
+// Your server should listen on the port provided by the first argument to your program.
+
+// my solution:
+// var http = require('http');
+// var map = require('through2-map');
+// var port = process.argv[2];
+
+// var server = http.createServer(function(request, response) {
+//   if (request.method == "POST") {
+//     request.pipe(map (function (chunk) {
+//       return chunk.toString().toUpperCase();
+//     })).pipe(response);
+//   }
+// });
+// server.listen(port);
+
+// learnyounode solution:
+// var http = require('http')
+// var map = require('through2-map')
+
+// var server = http.createServer(function (req, res) {
+//   if (req.method != 'POST')
+//     return res.end('send me a POST\n')
+
+//   req.pipe(map(function (chunk) {
+//     return chunk.toString().toUpperCase()
+//   })).pipe(res)
+// })
+
+// server.listen(Number(process.argv[2]))
